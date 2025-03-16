b24505097426fc3b98bbe20f810965a22fd0c076
sb-malo <subrotochondromalo@gmail.com>
2025-03-16 22:09:54 +0500

    pluse

---
+++ script.js
@@ -0,0 +1,16 @@
+function myfun(){
+    let Design = document.getElementById("stl");
+    Design.style.visibility = 'visible';
+}
+
+function myfun2(){
+    let Design = document.getElementById("stl");
+    Design.style.visibility = 'hidden';
+}
+
+function myfun3(){
+    let Design = document.getElementById("allbg");
+    
+    Design.style.visibility = 'visible';
+}
+
